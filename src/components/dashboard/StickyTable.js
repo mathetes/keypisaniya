55b4c0620b47493ad04f76f9a3766101e015bbb7
mathetes <nsk.roman@gmail.com>
2017-11-14 16:40:08 +0700

    TypeError: Cannot read property 'Provider' of undefined

---
+++ src/components/dashboard/StickyTable.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Table } from 'reactabular-table';
+import * as Sticky from 'reactabular-sticky';
+
+import { generateRows } from '../helpers/';
+
+
+const schema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    },
+    product: {
+      type: 'string'
+    },
+    company: {
+      type: 'string'
+    },
+    age: {
+      type: 'integer'
+    }
+  },
+  required: ['id', 'name', 'product', 'company', 'age']
+};
+const rows = generateRows(100, schema);
+
+const columns = [
+  {
+    property: 'name',
+    props: {
+      style: { minWidth: 300 }
+    },
+    header: {
+      label: 'Name'
+    }
+  },
+  {
+    property: 'age',
+    props: {
+      style: { minWidth: 100 }
+    },
+    header: {
+      label: 'Age'
+    }
+  },
+  {
+    property: 'company',
+    props: {
+      style: { minWidth: 400 }
+    },
+    header: {
+      label: 'Company'
+    }
+  },
+  {
+    property: 'product',
+    props: {
+      style: { minWidth: 400 }
+    },
+    header: {
+      label: 'Product'
+    }
+  }
+];
+
+class StickyTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rows,
+      columns
+    };
+
+    this.tableHeader = null;
+    this.tableBody = null;
+  }
+  componentDidMount() {
+    // We have refs now. Force update to get those to Header/Body.
+    this.forceUpdate();
+  }
+  render() {
+    const { rows, columns } = this.state;
+
+    return (
+      <Table.Provider
+        className="pure-table pure-table-striped"
+        columns={columns}
+      >
+        <Sticky.Header
+          style={{
+            maxWidth: 800
+          }}
+          ref={tableHeader => {
+            this.tableHeader = tableHeader && tableHeader.getRef();
+          }}
+          tableBody={this.tableBody}
+        />
+
+        <Sticky.Body
+          rows={rows}
+          rowKey="id"
+          style={{
+            maxWidth: 800,
+            maxHeight: 400
+          }}
+          ref={tableBody => {
+            this.tableBody = tableBody && tableBody.getRef();
+          }}
+          tableHeader={this.tableHeader}
+        />
+      </Table.Provider>
+    );
+  }
+}
+
+export default StickyTable;
--- src/containers/KeyparamsPage.js
+++ src/containers/KeyparamsPage.js
@@ -1,106 +1,14 @@
 import React, { Component } from 'react';
-import * as Table from 'reactabular-table';
+import StickyTable from '../components/dashboard/StickyTable';
 import globalStyles from '../styles';
 
-const rows = [
-  {
-    id: 100,
-    name: 'John',
-    tools: {
-      hammer: true
-    },
-    country: 'fi'
-  },
-  {
-    id: 101,
-    name: 'Jack',
-    tools: {
-      hammer: false
-    },
-    country: 'dk'
-  },
-  {
-    id: 102,
-    name: 'Roman',
-    tools: {
-      hammer: true
-    },
-    country: 'ru'
-  },
-  {
-    id: 103,
-    name: 'Jack',
-    tools: {
-      hammer: false
-    },
-    country: 'dk'
-  },
-];
-const countries = {
-  fi: 'Finland',
-  dk: 'Denmark',
-  ru: 'Russia'
-};
-
-const columns = [
-  {
-    property: 'name',
-    header: {
-      label: 'Name',
-      transforms: [
-        label => ({
-          onClick: () => alert(`clicked ${label}`)
-        })
-      ]
-    }
-  },
-  {
-    property: 'tools',
-    header: {
-      label: 'Active',
-      transforms: [
-        label => ({
-          onClick: () => alert(`clicked ${label}`)
-        })
-      ]
-    },
-    cell: {
-      formatters: [
-        tools => tools.hammer ? 'Hammertime' : 'nope'
-      ]
-    }
-  },
-  {
-    property: 'country',
-    header: {
-      label: 'Country',
-      transforms: [
-        label => ({
-          onClick: () => alert(`clicked ${label}`)
-        })
-      ]
-    },
-    cell: {
-      formatters: [
-        country => countries[country]
-      ]
-    }
-  },
-];
 
 class Keyparams extends Component {
     render() {
       return (
         <div>
           <h3 style={globalStyles.navigation}>Главная / Панель управления</h3>
-          <Table.Provider
-            className="pure-table pure-table-striped"
-            columns={columns}
-          >
-            <Table.Header />
-
-            <Table.Body rows={rows} rowKey="id" />
-          </Table.Provider>
+      <StickyTable />
 
         </div>
       );
